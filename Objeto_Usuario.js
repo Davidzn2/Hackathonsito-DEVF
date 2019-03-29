bd883165fd83a80df778d07dafea29fc7b8a3d3e
Moshe Shabes <moshnaf@gmail.com>
2019-03-28 21:35:28 -0600

    Se pone en el método sumaPuntos para imprimir en pantalla, también llamada de HTML a JS para detectar lo que se presiona, falta corregir acción al presional el boton

--- Objeto_Usuario.js
+++ Objeto_Usuario.js
@@ -1,41 +1,37 @@
+var input = document.getElementById("valorComentario")
+var comentario = document.getElementById("comentario")
 class usuario{
     constructor(id, nombre, celular, delegacion, puntos, nivel){
         this.id = id
         this.nombre = nombre,
         this.celular = celular,
         this.delegacion = delegacion,
-        this.puntos = 0,
-        this.nivel = ""
+        this.puntos = puntos,
+        this.nivel = nivel
     }
-
     sumaPuntos(mision)
     {
-        let puntosASumar = 0
-
+        let puntosASumar;
         switch(mision)
         {
             case 'like':
             puntosASumar = 1
             break;
-
             case 'video':
             puntosASumar = 10
             break;
-
             case 'comentario':
             puntosASumar = 5
             break;
-
             case 'mision_cumplida':
             puntosASumar = 20
             break;
-
             case 'compartir_video':
             puntosASumar = 20
         }
-
         this.puntos += puntosASumar
+        input.innerHTML = this.puntos;
     }
 }
-
-var david = new usuario(1, 'David', 5555, 'MH')
+var david = new usuario(1, 'David', 55646, "MH", 5000, "Oro")
+comentario.addEventListener("click", david.sumaPuntos("comentario"))
