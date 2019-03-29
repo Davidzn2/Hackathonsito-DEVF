247aa6ec41b0b884595dea9be4f7e59863b7fd1a
davidzn2 <davidzn2@gmail.com>
2019-03-28 19:53:32 -0600

    INitial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser');
+const path = require('path');
+const PORT = 8000
+
+
+//Configuración para poder recibir el body
+app.use(bodyParser.json());
+
+
+app.use('/static', express.static(__dirname + '/static'));
+
+app.listen(PORT, () =>{
+    console.log(`Tu servidor esta corriendo en el puerto ${PORT}`);
+});
